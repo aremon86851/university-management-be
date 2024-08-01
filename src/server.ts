29e296067a493edb24d9023b138648057302e913
aremon-coderower <aremon.rahman@coderower.com>
2024-08-01 22:31:04 +0600

    mongoose add

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`😊Database connected successfully😊}`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`😊Database connected successfully😊}`)
     app.listen(config.port, () => {
-      console.log(`Application app listening on port ${config.port}`);
-    });
+      console.log(`Application app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-main();
+main()
