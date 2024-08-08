a676fded35ce49437652f38853e46edb28c9c3c0
aremon-coderower <aremon.rahman@coderower.com>
2024-08-08 22:17:39 +0600

    done setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
@@ -10,36 +9,31 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
 export default [
-  ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ),
-  {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-      prettier,
-    },
+    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
 
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
 
-      parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
+            parser: tsParser,
+            ecmaVersion: 12,
+            sourceType: "module",
+        },
 
-    rules: {
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        rules: {
+            "@typescript-eslint/no-unused-vars": "error",
+            "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        },
     },
-  },
 ];
