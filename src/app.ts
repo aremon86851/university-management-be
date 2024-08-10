ce8ebee66711006e47ebabf33c5d3d596151807b
aremon-coderower <aremon.rahman@coderower.com>
2024-08-10 22:31:05 +0600

    [DONE] add lint-stage and add prettier and lint check

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 
 app.use(cors());
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
 
-//11.6
+//11.7
